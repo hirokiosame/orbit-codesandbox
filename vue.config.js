0e27e97d4823bb6d41c2a3ac8dabcf2a04b31d46
Hiroki Osame <hiroki@weebly.com>
2019-01-29 16:11:15 -0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   devServer: {
     public: "http://0.0.0.0"
+  },
+  configureWebpack: {
+  	externals: [
+  		/^orbit-ui/,
+  	],
+    output: {
+    	libraryTarget: 'amd',
+    },
   }
 };
