ff7bfbcda2eae77608cdde3eb6379681669aa9da
Hiroki Osame <hiroki@weebly.com>
2019-01-29 16:41:15 -0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  devServer: {
-  	allowedHosts: [
-  		'.sse.codesandbox.io',
-  	],
-    public: "http://0.0.0.0"
-  },
-  configureWebpack: {
-  	externals: [
-  		/^orbit-ui/,
-  	],
-  	output: {
-	  	libraryTarget: 'amd',
-		library: 'App',
-  	},
-  },
+	devServer: {
+		allowedHosts: [
+			'.sse.codesandbox.io',
+		],
+		public: "http://0.0.0.0"
+	},
+	configureWebpack: {
+		externals: [
+			/^orbit-ui/,
+		],
+		output: {
+			libraryTarget: 'amd',
+			library: 'App',
+		},
+	},
 };
